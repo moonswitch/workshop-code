f5f6f745114ce379344257af1d4561984e91c79b
Jeff French <jeff.french@moonswitch.com>
2023-04-12 13:00:13 -0500

    Fix AZ data source refs

--- eks/01-vpc-track/challenge-solve/vpc.tf
+++ eks/01-vpc-track/challenge-solve/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "vpc-${var.environment}"
   cidr = local.vpc_cidr
 
-  azs                 = [data.aws_availability_zones.aws[0], data.aws_availability_zones.aws[1], data.aws_availability_zones.aws[2]]
+  azs                 = [data.aws_availability_zones.azs[0], data.aws_availability_zones.azs[1], data.aws_availability_zones.azs[2]]
   private_subnets     = ["10.0.1.0/19", "10.0.32.0/19", "10.0.64.0/19"]
   public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   database_subnets    = ["10.0.151.0/24", "10.0.152.0/24", "10.0.153.0/24"]
