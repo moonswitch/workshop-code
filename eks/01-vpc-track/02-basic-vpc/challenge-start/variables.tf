26bba95474e729cfe16090d715b7f9a98a2b7939
Jeff French <jeff.french@moonswitch.com>
2023-04-14 10:08:08 -0500

    Symlink challenge-start to previous challenge-solve

--- eks/01-vpc-track/02-basic-vpc/challenge-start/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  type    = string
-  default = "Dev"
-}
-
-variable "region" {
-  type    = string
-  default = "us-east-2"
-}
--- eks/01-vpc-track/02-basic-vpc/challenge-start/vpc.tf
+++
@@ -1,4 +0,0 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.1"
-}
