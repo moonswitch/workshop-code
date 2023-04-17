eaf0f898b83d4881e928144ecb07471799c8e818
Jeff French <jeff.french@moonswitch.com>
2023-04-17 16:17:18 -0500

    Remove redundant tags

--- eks/01-vpc-track/02-basic-vpc/challenge-solve/vpc.tf
+++ eks/01-vpc-track/02-basic-vpc/challenge-solve/vpc.tf
@@ -8,9 +8,4 @@ module "vpc" {
   azs             = [data.aws_availability_zones.azs.names[0], data.aws_availability_zones.azs.names[1], data.aws_availability_zones.azs.names[2]]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform   = "true"
-    Environment = var.environment
-  }
 }
