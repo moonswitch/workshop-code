4b1beefa06bcfb121cacfac0a0b12ee973672d3c
Jeff French <jeff.french@moonswitch.com>
2023-04-17 12:33:17 -0500

    Simplify Basic VPC challenge code

--- eks/01-vpc-track/02-basic-vpc/challenge-solve/vpc.tf
+++ eks/01-vpc-track/02-basic-vpc/challenge-solve/vpc.tf
@@ -3,16 +3,11 @@ module "vpc" {
   version = "4.0.1"
 
   name = "vpc-${local.env_name}"
-  cidr = local.vpc_cidr
+  cidr = "10.0.0.0/16"
 
   azs             = [data.aws_availability_zones.azs.names[0], data.aws_availability_zones.azs.names[1], data.aws_availability_zones.azs.names[2]]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # Nat gateways
-  enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
 
   tags = {
     Terraform   = "true"
